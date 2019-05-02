439795194ba28fb22b868d2dfc41f38dad53c858
MarineLH <marine.lhuillier@epsi.fr>
2019-05-02 16:07:55 +0200

    add bootstrap template for first interface

--- connexion.php
+++ connexion.php
@@ -1,110 +1,25 @@
-<?php
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="assets/css/connexion.css">
 
-include('includes/menu.php');
-?>
+<!------ Include the above in your HEAD tag ---------->
 
-<head>
+<div class="wrapper fadeInDown">
+    <div id="formContent">
+        <!-- Tabs Titles -->
 
-    <meta charset="utf-8">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>PKI</title>
-
-    <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
-
-    <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
-
-
-
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Fira+Sans|Roboto:300,400|Questrial|Satisfy">
-
-    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
-
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-
-    <link rel="stylesheet" type="text/css" href="css/animate.css">
-
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-
-
-
-</head>
-
-
-
-
-
-<body>
-
-
-
-<div class="margeConnexion">
-
-    <div style="margin-top: -5%" class="container">
-
-
-
-        <div class="row form_connexion">
-
-            <div class="heading_connexion" style="text-align: center">
-
-                <table style="width: 100%">
-
-                    <tbody>
-
-                    <td>
-
-                        <p style="font-size: 4em;"><strong>CONNEXION</strong></p>
-
-                    </td>
-
-                    </tbody>
-
-                </table>
-
-            </div>
-
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-10 col-lg-offset-1">
-
-                    <form action="check_connexion.php"
-
-                          method="POST">
-
-                        <div class="form-group">
-
-                            <label for="username">Identifiant :</label>
-
-                            <input type="text" class="form-control" id="identifiant" name="identifiant"
-
-                                   placeholder="Identifiant" required>
-
-                        </div>
-
-                        <div class="form-group">
-
-                            <label for="password">Mot de passe:</label>
-
-                            <input type="password" class="form-control" id="password" name="password"
-
-                                   placeholder="Mot de passe"
-
-                                   required>
-
-                        </div>
-
-                        <button style="float: right" type="submit" class="btn btn-default col-md-2">Connexion</button>
-
-                    </form>
-
-            </div>
-
+        <!-- Icon -->
+        <div class="fadeIn first">
+           <h1>Connexion</h1>
         </div>
 
+        <!-- Login Form -->
+        <form action="check_connexion.php" method="POST">
 
-
+            <input type="text" id="identifiant" class="fadeIn second" name="identifiant" placeholder="identifiant" required>
+            <input type="password" id="password" class="fadeIn third" name="password" placeholder="mot de passe" required>
+            <input type="submit" class="fadeIn fourth" value="Se connecter">
+        </form>
     </div>
-
 </div>
-
-</body>
--- index.php
+++ index.php
@@ -1,64 +1,321 @@
 <?php
-include ('includes/menu.php');
-include('includes/connexion.php');
+    include('includes/connexion.php');
+
 ?>
-<html>
-<form method="post" action="uploadCertif.php" enctype="multipart/form-data">
-    <input type="file" name="file">
-    <button type="submit"> Envoyer</button>
-</form>
-</html>
+<!doctype html>
+<html lang="fr">
+
+<head>
+	<title>MTVP PKI</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<!-- VENDOR CSS -->
+	<link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/vendor/linearicons/style.css">
+	<link rel="stylesheet" href="assets/vendor/chartist/css/chartist-custom.css">
+	<!-- MAIN CSS -->
+	<link rel="stylesheet" href="assets/css/main.css">
+	<!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
+	<link rel="stylesheet" href="assets/css/demo.css">
+	<!-- GOOGLE FONTS -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+	<!-- ICONS -->
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+</head>
+
+<?php if (!isset($_SESSION)) {
+    session_start();
+    }
+?>
 
 <?php
 
-session_start();
+    if (empty($_SESSION['connected'])) {
 
-$pers = $bdd->prepare('SELECT admin FROM personne WHERE id = :id ');
+        $login = 0;
 
-$pers->execute(array(
-    'id' => $_SESSION['id'],
-));
+    } else {
 
+        $login = 1;
+        $pers = $bdd->prepare('SELECT admin FROM personne WHERE id = :id ');
+        $pers->execute(array(
+            'id' => $_SESSION['id'],
+        ));
+        $personne = $pers->fetch();
+        if ($personne['admin'] == 1) {
+            $cert = $bdd->prepare('SELECT path_certificate, nom, prenom FROM certificates INNER JOIN personne ON personne.id = certificates.id_demandeur');
 
-$personne = $pers->fetch();
+            $cert->execute();
+        } else {
+            $cert = $bdd->prepare('SELECT path_certificate, nom, prenom FROM certificates INNER JOIN personne ON personne.id = certificates.id_demandeur WHERE id = :id');
 
-if ($personne['admin'] == 1) {
-    $cert = $bdd->prepare('SELECT path_certificate, nom, prenom FROM certificates INNER JOIN personne ON personne.id = certificates.id_demandeur');
-
-    $cert->execute();
-} else {
-    $cert = $bdd->prepare('SELECT path_certificate, nom, prenom FROM certificates INNER JOIN personne ON personne.id = certificates.id_demandeur WHERE id = :id');
-
-    $cert->execute(array(
-            'id' => $_SESSION['id']
-    ));
-}
-
-
+            $cert->execute(array(
+                'id' => $_SESSION['id']
+            ));
+        }
+    }
 ?>
 
-<table>
-    <thead>
 
-    <tr>
-        <th>Nom du certificat</th>
-        <th>Nom du demandeur</th>
-        <th>Prenom du demandeur</th>
-        <th>Accepter</th>
-        <th>Refuser</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    while ($lesCerts = $cert->fetch()) {
-        ?>
-    <tr>
-        <td><strong><?php echo $lesCerts['path_certificate'] ?></strong></td>
-        <td><?php echo $lesCerts['nom']?></td>
-        <td><?php echo $lesCerts['prenom']?></td>
-        <td><a href="signing.php?csr=request/<?php echo $lesCerts['path_certificate']?>">Accepter</a></td>
-        <td><button>Refuser</button></td>
-    </tr>
-    <?php } ?>
-    </tbody>
-</table>
+<body>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+		<!-- NAVBAR -->
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="brand">
+				<b>MTVP PKI</b>
+			</div>
+			<div class="container-fluid">
+				<div id="navbar-menu">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="dropdown">
+								<li><a href="deconnexion.php"><i class="lnr lnr-exit"></i> <span>Logout</span></a></li>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="">
+			<!-- MAIN CONTENT -->
+			<div class="main-content">
+				<div class="container-fluid">
+					<!-- OVERVIEW -->
+					<div class="panel panel-headline">
+						<div class="panel-heading">
+							<h3 class="panel-title">Importer une Demande de Certificat (CSR)</h3>
+						</div>
+						<div class="panel-body">
+							<div class="row">
+								<div class="col-md-6">
+                                    <form method="post" action="uploadCertif.php" enctype="multipart/form-data">
+                                        <div class="form-group">
+                                            <input type="file" class="form-control" name="file" required>
+                                            <span class="form-control" >
+                                                <span>FQDN : </span>
+                                                <input type="text" name="fqdn" placeholder="exemple.fr" required>
+                                            </span>
+                                            <br>
+                                            <span class="col-md-9"></span>
+                                            <button type="submit" class="col-md-3"> Envoyer</button>
+                                        </div>
+                                    </form>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- END OVERVIEW -->
+					<div class="row">
+						<div class="col-md-12">
+							<!-- RECENT PURCHASES -->
+							<div class="panel">
+								<div class="panel-heading">
+									<h3 class="panel-title">Demandes de Certificats</h3>
+									<div class="right">
+										<button type="button" class="btn-toggle-collapse"><i class="lnr lnr-chevron-up"></i></button>
+										<button type="button" class="btn-remove"><i class="lnr lnr-cross"></i></button>
+									</div>
+								</div>
+								<div class="panel-body no-padding">
+									<table class="table table-striped">
+										<thead>
+											<tr>
+												<th>Nom du certificat</th>
+												<th>Nom du demandeur</th>
+												<th>Prénom du demandeur</th>
+                                                <th>Accepter</th>
+												<th>Statut</th>
+											</tr>
+										</thead>
+										<tbody>
+                                            <?php
+                                            while ($lesCerts = $cert->fetch()) {
+                                                ?>
+                                                <tr>
+                                                    <td><strong><?php echo $lesCerts['path_certificate'] ?></strong></td>
+                                                    <td><?php echo $lesCerts['nom']?></td>
+                                                    <td><?php echo $lesCerts['prenom']?></td>
+                                                    <td><button><a href="signing.php?csr=request/<?php echo $lesCerts['path_certificate']?>">Accepter</a></button></td>
+                                                    <td><span class="label label-success">complet</span></td>
+                                                </tr>
+                                            <?php } ?>
+										</tbody>
+									</table>
+								</div>
+							</div>
+							<!-- END RECENT PURCHASES -->
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- END MAIN CONTENT -->
+		</div>
+		<!-- END MAIN -->
+		<div class="clearfix"></div>
+		<footer>
+			<div class="container-fluid">
+				<p class="copyright">&copy; 2019 MTVP PKI</p>
+			</div>
+		</footer>
+	</div>
+	<!-- END WRAPPER -->
+	<!-- Javascript -->
+	<script src="assets/vendor/jquery/jquery.min.js"></script>
+	<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+	<script src="assets/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+	<script src="assets/vendor/chartist/js/chartist.min.js"></script>
+	<script src="assets/scripts/klorofil-common.js"></script>
+	<script>
+        function bs_input_file() {
+            $(".input-file").before(
+                function() {
+                    if ( ! $(this).prev().hasClass('input-ghost') ) {
+                        var element = $("<input type='file' class='input-ghost' style='visibility:hidden; height:0'>");
+                        element.attr("name",$(this).attr("name"));
+                        element.change(function(){
+                            element.next(element).find('input').val((element.val()).split('\\').pop());
+                        });
+                        $(this).find("button.btn-choose").click(function(){
+                            element.click();
+                        });
+                        $(this).find("button.btn-reset").click(function(){
+                            element.val(null);
+                            $(this).parents(".input-file").find('input').val('');
+                        });
+                        $(this).find('input').css("cursor","pointer");
+                        $(this).find('input').mousedown(function() {
+                            $(this).parents('.input-file').prev().click();
+                            return false;
+                        });
+                        return element;
+                    }
+                }
+            );
+        }
+        $(function() {
+            bs_input_file();
+        });
+
+	$(function() {
+		var data, options;
+
+		// headline charts
+		data = {
+			labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+			series: [
+				[23, 29, 24, 40, 25, 24, 35],
+				[14, 25, 18, 34, 29, 38, 44],
+			]
+		};
+
+		options = {
+			height: 300,
+			showArea: true,
+			showLine: false,
+			showPoint: false,
+			fullWidth: true,
+			axisX: {
+				showGrid: false
+			},
+			lineSmooth: false,
+		};
+
+		new Chartist.Line('#headline-chart', data, options);
+
+
+		// visits trend charts
+		data = {
+			labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+			series: [{
+				name: 'series-real',
+				data: [200, 380, 350, 320, 410, 450, 570, 400, 555, 620, 750, 900],
+			}, {
+				name: 'series-projection',
+				data: [240, 350, 360, 380, 400, 450, 480, 523, 555, 600, 700, 800],
+			}]
+		};
+
+		options = {
+			fullWidth: true,
+			lineSmooth: false,
+			height: "270px",
+			low: 0,
+			high: 'auto',
+			series: {
+				'series-projection': {
+					showArea: true,
+					showPoint: false,
+					showLine: false
+				},
+			},
+			axisX: {
+				showGrid: false,
+
+			},
+			axisY: {
+				showGrid: false,
+				onlyInteger: true,
+				offset: 0,
+			},
+			chartPadding: {
+				left: 20,
+				right: 20
+			}
+		};
+
+		new Chartist.Line('#visits-trends-chart', data, options);
+
+
+		// visits chart
+		data = {
+			labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+			series: [
+				[6384, 6342, 5437, 2764, 3958, 5068, 7654]
+			]
+		};
+
+		options = {
+			height: 300,
+			axisX: {
+				showGrid: false
+			},
+		};
+
+		new Chartist.Bar('#visits-chart', data, options);
+
+
+		// real-time pie chart
+		var sysLoad = $('#system-load').easyPieChart({
+			size: 130,
+			barColor: function(percent) {
+				return "rgb(" + Math.round(200 * percent / 100) + ", " + Math.round(200 * (1.1 - percent / 100)) + ", 0)";
+			},
+			trackColor: 'rgba(245, 245, 245, 0.8)',
+			scaleColor: false,
+			lineWidth: 5,
+			lineCap: "square",
+			animate: 800
+		});
+
+		var updateInterval = 3000; // in milliseconds
+
+		setInterval(function() {
+			var randomVal;
+			randomVal = getRandomInt(0, 100);
+
+			sysLoad.data('easyPieChart').update(randomVal);
+			sysLoad.find('.percent').text(randomVal);
+		}, updateInterval);
+
+		function getRandomInt(min, max) {
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}
+
+	});
+	</script>
+</body>
+
+</html>
